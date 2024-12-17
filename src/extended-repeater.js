01eaa4197506a49e152d4b99dcdcfdcdc4fb62c6
Glebasik58 <gglebasemil@gmail.com>
2024-12-17 11:06:58 +0500

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,23 +1,22 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
- *
- * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // Обработка значения addition и его повторов
+  const addition = options.hasOwnProperty('addition') ? String(options.addition) : '';
+  const additionRepeatTimes = options.hasOwnProperty('additionRepeatTimes') ? options.additionRepeatTimes : 1;
+  const additionSeparator = options.hasOwnProperty('additionSeparator') ? options.additionSeparator : '|';
+
+  // Создание строки дополнения (addition) с учетом повторов и разделителя
+  const repeatedAddition = Array(additionRepeatTimes).fill(addition).join(additionSeparator);
+
+  // Обработка значений строки (str) и ее повторов
+  const repeatTimes = options.hasOwnProperty('repeatTimes') ? options.repeatTimes : 1;
+  const separator = options.hasOwnProperty('separator') ? options.separator : '+';
+
+  // Создание основной строки с учетом повторов и разделителя
+  const repeatedString = Array(repeatTimes).fill(str + repeatedAddition).join(separator);
+
+  return repeatedString;
 }
 
 module.exports = {
